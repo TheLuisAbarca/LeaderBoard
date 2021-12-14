a505239ebb6c360ffae456c9ee556d988dd0d32d
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-14 16:16:51 -0500

    Add style like wireframe indicated.

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
+import '../css/bootstrap.min.css';
+import '../css/style.css';
